83f3b587b11fce56460ee99d696e2074c85d642c
Hazim Hafis <hazimhf.dev@gmail.com>
2023-06-09 03:26:19 +0800

    declare static path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { isoImport } from 'vite-plugin-iso-import';
+import { resolve } from 'path';
 
 
 export default defineConfig({
-	plugins: [sveltekit(), isoImport()]
+	plugins: [sveltekit(), isoImport()],
+    resolve: {
+        alias: { 
+            $fonts: resolve('./static/fonts'),
+        }
+    }
 });
