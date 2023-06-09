972812a21e998715add79cf14dd8ddca3eca8372
Hazim Hafis <hazimhf.dev@gmail.com>
2023-06-10 00:47:37 +0800

    removed path config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { isoImport } from 'vite-plugin-iso-import';
-import { resolve } from 'path';
 
 
 export default defineConfig({
